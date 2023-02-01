367546694aa09266a9abbd891c98d34423f5d3d7
Barry Buitelaar <barry.buitelaar@inqdo.com>
2023-02-01 11:16:44 +0100

    no message

--- exec-code-coverage.sh
+++ exec-code-coverage.sh
@@ -1,4 +1,40 @@
 #!/usr/bin/env bash
 
-docker-compose exec inqdo-tools pytest --cov-config=.coveragerc --cov=. --cov-report html
-open tests/htmlcov/index.html
+GREEN=`tput setaf 2`
+RED=`tput setaf 1`
+YELLOW=`tput setaf 3`
+CYAN=`tput setaf 6`
+C=`tput sgr0`
+
+logger_ok () {
+    echo ""
+    echo "${GREEN}#############################################${C}"
+    echo $1
+    echo "${GREEN}#############################################${C}"
+    echo ""
+}
+
+logger_notok () {
+    echo ""
+    echo "${RED}#############################################${C}"
+    echo $1
+    echo "${RED}#############################################${C}"
+    echo ""
+}
+
+if [[ $# -ge 1 ]]; then
+    ACTION=$1
+
+    if [ $ACTION == "html" ]; then
+      logger_ok  "${CYAN}Action = $ACTION ${C}"
+
+      docker-compose exec inqdo-tools pytest --cov-config=.coveragerc --cov=. --cov-report html
+      open tests/htmlcov/index.html
+    else
+      logger_notok "Provide argument ${YELLOW}html${C}${RED}, for determine the report${C}"
+    fi
+
+else
+    docker-compose exec -T inqdo-tools pytest --cov-fail-under=50 --cov=.
+fi
+
--- tests/mocks/cognito.py
+++ tests/mocks/cognito.py
@@ -95,3 +95,33 @@ def cognito_create_users_in_groups(
         cognito_client.admin_add_user_to_group(
             UserPoolId=user_pool_id, GroupName=f"test_group_{user}", Username=user
         )
+
+
+@pytest.fixture
+def cognito_create_multiple_users_groups(
+    cognito_client, cognito_create_user_pool, cognito_create_group
+):
+    user_pool_id = cognito_create_user_pool["UserPool"]["Id"]
+
+    for index in range(150):
+        cognito_client.create_group(
+            UserPoolId=user_pool_id,
+            GroupName=f"test_group_{index}",
+            Description=f"test_group_{index}",
+        )
+        cognito_client.admin_create_user(
+            UserPoolId=user_pool_id,
+            Username=f"user-{index}",
+            TemporaryPassword="Passw0rd!",
+            UserAttributes=[
+                {"Name": "email", "Value": f"{index}@example.com"},
+                {"Name": "email_verified", "Value": "true"},
+            ],
+        )
+
+        cognito_client.admin_add_user_to_group(
+            UserPoolId=user_pool_id, GroupName="test_group", Username=f"user-{index}"
+        )
+        cognito_client.admin_add_user_to_group(
+            UserPoolId=user_pool_id, GroupName=f"test_group_{index}", Username=f"user-{index}"
+        )
