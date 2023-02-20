e25fa1f998365a7bb7f1e0cff0ab439c871f38f4
Barry Buitelaar <barry.buitelaar@inqdo.com>
2023-02-20 15:43:09 +0100

    Introduced collect batch

--- tests/unit/utils/test_logger.py
+++ tests/unit/utils/test_logger.py
@@ -6,8 +6,17 @@ from inqdo_tools.utils.logger import SaveSequenceLogger, SequenceLogger
 
 def test_collect_logs():
     seq_logger = SequenceLogger("Test Logger")
-    seq_logger.collect_logs("Log 1", "Value 1")
-    seq_logger.collect_logs("Log 2", "Value 2")
+    seq_logger.collect_log("Log 1", "Value 1")
+    seq_logger.collect_log("Log 2", "Value 2")
+
+    assert len(seq_logger.logs) == 2
+    assert seq_logger.logs[0] == ("[1] Log 1", "Value 1")
+    assert seq_logger.logs[1] == ("[2] Log 2", "Value 2")
+
+
+def test_collect_batch():
+    seq_logger = SequenceLogger("Test Logger")
+    seq_logger.collect_batch(batch_list=[("Log 1", "Value 1"), ("Log 2", "Value 2")])
 
     assert len(seq_logger.logs) == 2
     assert seq_logger.logs[0] == ("[1] Log 1", "Value 1")
@@ -16,7 +25,7 @@ def test_collect_logs():
 
 def test_sequence_logger_with_long_name():
     seq_logger = SequenceLogger("Test Logger with a Very Long Name")
-    seq_logger.collect_logs("Log 1", "Value 1")
+    seq_logger.collect_log("Log 1", "Value 1")
 
     captured_output = io.StringIO()
     sys.stdout = captured_output
@@ -30,7 +39,7 @@ def test_sequence_logger_with_long_name():
 
 def test_save_sequence_logger_with_long_name():
     seq_logger = SequenceLogger("Test Logger with a Very Long Name")
-    SaveSequenceLogger(sequence_logger=seq_logger).collect_logs("Log 1", "Value 1")
+    SaveSequenceLogger(sequence_logger=seq_logger).collect_log("Log 1", "Value 1")
 
     captured_output = io.StringIO()
     sys.stdout = captured_output
