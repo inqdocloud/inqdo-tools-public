6fc2a580e8e3c9a8ee6f82d118a0a9b0601de4cb
Lotte Diesveld <lotte.diesveld@icloud.com>
2023-02-22 11:20:40 +0100

    Tests added for config/utils

---
+++ tests/unit/config/test_utils.py
@@ -0,0 +1,67 @@
+from inqdo_tools.config.utils import delete_config_rule, update_config_rules, construct_managed_meta_data
+
+
+def test_delete_config_rule():
+    config_rule = { "Name" : "TestRule" }
+    
+    config_rules = []
+    config_rules.append(config_rule)
+    
+    event: str
+    event = { "Name" : "TestRule" }
+    
+    result = delete_config_rule(config_rules=config_rules, event=event)
+    
+    assert len(result) == 0
+
+
+def test_update_config_rules():
+    config_rule = { "Name" : "TestRule" }
+    config_rules = []
+    config_rules.append(config_rule)
+    event: str
+    event = { "Name" : "TestRule" }
+    
+    result = update_config_rules(config_rules=config_rules, event=event, enabled=True)
+    
+    assert len(result) == 1
+
+
+def test_construct_managed_meta_data_fullevent():
+    event = { 
+        "Name": "",
+        "ExcludedAccounts": "Test",
+        "Enabled" : True,
+        "InputParameters" : {
+            "Test": "Test"
+        }
+    }
+    
+    result = construct_managed_meta_data(event=event)
+    
+    assert result == {'InputParameters': '{"Test": "Test"}'}
+
+def test_construct_managed_meta_data_missing_excluded_accounts():
+    event = { 
+        "Name": "",
+        "Enabled" : True,
+        "InputParameters" : {
+            "Test": "Test"
+        }
+    }
+    result = construct_managed_meta_data(event=event)
+    
+    assert result == {'InputParameters': '{"Test": "Test"}'}
+
+def test_construct_managed_meta_data_missing_enabled():
+    event = { 
+        "Name": "",
+        "ExcludedAccounts": "Test",
+        "InputParameters" : {
+            "Test": "Test"
+        }
+    }
+    
+    result = construct_managed_meta_data(event=event)
+    
+    assert result == {'InputParameters': '{"Test": "Test"}'}
